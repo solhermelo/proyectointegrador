a884d7445d98c99e7eca62bfc2e6557c0adc7f9f
franzunino <50157868+franzunino@users.noreply.github.com>
2019-06-10 12:04:26 -0300

    cambio con require

--- PI_Todos/index.php
+++ PI_Todos/index.php
@@ -1,3 +1,8 @@
+<?php
+
+ ?>
+
+
 <!DOCTYPE html>
 <html>
 <!-- Beautify -->
@@ -11,25 +16,12 @@
 </head>
 
 <body>
-    <header>
-        <!--comienzo barra de navegador -->
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light fixed-top">
-            <a class="navbar-brand" href="#">¿Quien juega?</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="navbar-nav ml-auto">
-                    <a class="nav-item nav-link active" href="index.php">Inicio<span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link active" href="FAQ.php">Preguntas frecuentes</a>
-                    <a class="nav-item nav-link active" href="iniciosesion.php">Iniciar sesion<span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link active" href="registracion.php">Registrarse</a>
-                </div>
-            </div>
-        </nav>
 
-    </header>
-    <!--fin barra de navegador -->
+<?php
+require_once("navbar.php");
+ ?>
+
+
 
     <section>
         <!--comienzo carrousel -->
--- PI_Todos/iniciosesion.php
+++ PI_Todos/iniciosesion.php
@@ -8,27 +8,10 @@
     <title>Inicio sesión</title>
   </head>
   <body>
-    <header>
-      <!--comienzo barra de navegador-->
-      <nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-ligh fixed-top">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">¿Quien juega?</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ml-auto">
-          <a class="nav-item nav-link active" href="index.php">Inicio<span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link active" href="FAQ.php">Preguntas frecuentes</a>
-          <a class="nav-item nav-link active" href="iniciosesion.php">Iniciar sesion<span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link active" href="registracion.php">Resgistrarse</a>
-        </div>
-      </div>
-  </nav>
-  </header>
-  <!--fin barra de navegador -->
+
+    <?php
+    require_once("navbar.php");
+     ?>
     <div class="login-box">
       <div class="left-box">
         <h2>Inicia sesión</h2>
