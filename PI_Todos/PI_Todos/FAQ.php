208163544c709b4117c8cfae7e017299a27b2f8b
solhermelo <50157862+solhermelo@users.noreply.github.com>
2019-06-11 00:03:21 -0300

    validacion datos registracion y log in
    lo de log in esta medio incompleto y falta ver si se guardan los usuarios que se registran

---
+++ PI_Todos/PI_Todos/FAQ.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!--Para ejecutar EMMET html:5, usar tab-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Esta etiqueta es para que me de propiedades responsive-->
+    <meta name="twitter:" content="">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>FAQ Football Academy</title>
+    <link rel="stylesheet" href="styles.css">
+
+  </head>
+
+  <body class="container2">
+
+    <?php require_once("navbar.php"); ?>
+    
+    <div class="div_1 shadow p-3 mb-5 bg-white rounded">
+          <h1 class="titulo">Preguntas Frecuentes</h1>
+          <br>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿En que zonas se alquilan canchas?</h2>
+          <p class="respuesta"> Por el momento contamos con alquiler de canchas de football 5 y 7 en Capital Federal y GBA. Próximamente podrán encontrarnos en Rosario, Córdoba y Bahía Blanca.</p>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿Como logro unirme a un equipo si nadie conoce como juego?</h2>
+          <p class="respuesta"> La idea es que subas tu perfil indicando posición en la que jugas, edad y habilidades. Podés invitar a otros amigos a que comenten como es jugar con vos asi nuestros suscriptores pueden conocerte mejor.</p>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿Como evalúo que jugador conviene sumar a mi equipo?</h2>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿De cuantos jugadores son los equipos que se arman? </h2>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿Cuanto tiempo pasa desde la inscipción hasta que te llama un equipo</h2>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿Que es "Jugador Amigo" y que es "Equipo Amigo"?</h2>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿Esta aplicaión funciona para otros deportes?</h2>
+          <h2 class="shadow-lg p-3 mb-2 bg-white rounded preguntas"> ¿Si un equipo me invita a jugar con ellos quien debe realizar el pago ellos o yo?</h2>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
---
+++ PI_Todos/PI_Todos/iniciosesion.php
@@ -0,0 +1,110 @@
+<?php
+  require_once("funciones.php");
+  if($_POST){
+    $errores = validar($_POST,'login');
+    if(count($errores) == 0){
+
+      $usuario = buscarPorEmail($_POST["email"]);
+
+      if($usuario == null){
+        $errores["email"]= "Usuario / Contraseña invalidos";
+      }else{
+        if(password_verify($_POST["password"],$usuario["password"])==false){
+          $errores["password"]="Usuario / Contraseña invalidos";
+        }else {
+
+          seteoUsuario($usuario,$_POST);
+          if(validarAcceso()){
+            header("location: index.php");
+            exit;
+          }else{
+            header("location: iniciosesion.php");
+            exit;
+          }
+
+        }
+      }
+
+      }
+  }
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="iniciosesion.css">
+    <link rel="stylesheet" href="styles.css">
+    <title>Inicio sesión</title>
+  </head>
+  <body>
+
+    <?php
+    require_once("navbar.php");
+     ?>
+    <div class="login-box">
+      <div class="left-box">
+        <?php if(isset($errores)):?>
+              <ul class="alert alert-danger">
+                <?php foreach ($errores as $key => $value) :?>
+                  <li><?=$value;?></li>
+                <?php endforeach;?>
+              </ul>
+        <?php endif;?>
+
+        <h2>Inicia sesión</h2>
+
+        <form class="" action="index.php" method="post">
+          <p>
+            <label for="email">
+              Email
+            </label>
+            <input id="email" type="email" name="email" value="<?=isset($errores['email'])? "":persistir("email") ;?>" placeholder="xxx@xmail.com">
+          </p>
+          <p>
+            <label for="pass">
+              Contraseña
+            </label>
+            <input id="pass" type="password" name="pass" value="">
+          </p>
+          <p>
+            <a class="olvidocontra" href="#">Olvidó su contraseña?</a>
+          </p>
+          <p>
+          <label for="recordarme">Recordarme</label>
+
+          <input type="checkbox" name="" value="">
+        </p>
+          <p>
+            <input type="submit" name="" value="Enviar">
+          </p>
+        </form>
+
+      </div>
+      <div class="right-box">
+        <span class="signin-with">Inicia sesión con Redes Sociales <br>
+        </span>
+        <button class="social facebook" type="button" name="button"> <img class="logo-fb" src="imagenes/logo-fb.jpg" alt="logo facebook">
+        Facebook
+        </button>
+        <br>
+
+        <button class="social google" type="button" name="button"> <img class="logo-google" src="imagenes/logo-google.jpg" alt="logo Google"> Google
+        </button>
+
+
+      </div>
+      <div class="sino">Sino
+      </div>
+
+
+    </div>
+
+
+  </body>
+</html>
