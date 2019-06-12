03f52f5e7713a6a37e1b417cad8afdb5c200e72e
josefinaloyola <50785115+josefinaloyola@users.noreply.github.com>
2019-06-11 22:24:47 -0300

    Add files via upload
    Archivos actualizados - Sprint #2

--- PI_Todos/index.php
+++ PI_Todos/index.php
@@ -11,7 +11,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
     <meta name="viewport" content="width=device-width" , "initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Proyecto</title>
 </head>
 
--- PI_Todos/registracion.php
+++ PI_Todos/registracion.php
@@ -20,8 +20,8 @@ if($_POST){
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="utf-8">
-    <link rel="stylesheet" href="registracion.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/registracion.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Registración</title>
   </head>
   <body>
