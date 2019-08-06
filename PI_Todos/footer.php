3804850043d1f93fc32a58b13a60a48f322beffa
josefinaloyola <jocloyola@hotmail.com>
2019-08-06 00:36:03 -0300

    Chiks aca paso el proyecto con varias modificaciones.

--- PI_Todos/footer.php
+++ PI_Todos/footer.php
@@ -1,20 +1,24 @@
 <footer class="footer">
     <a class="links" href="#">
-      <p>Quiénes somos?</p>
+      <p>¿Quiénes somos?</p>
     </a>
-    <br>
+      <br>
     <a class="links" href="#">
       <p>Preguntas frecuentes</p>
     </a>
-    <br>
+      <br>
     <a class="links" href="#">
       <p>Contacto</p>
     </a>
+
     <a href="#">
       <img class="img-footer" src="imagenes/logo-quienjuega.png" alt="logo">
     </a>
-    <h6 class="rrss">Encontranos en nuestras redes sociales</h6>
-    <a href="#">
-      <img src="imagenes/logo-fb" alt="">
-    </a>
+
+    <section>
+      <h6 class="rrss">ENCONTRANOS</h6>
+      <!--<img class="logo-fb" src="imagenes/logo-fb.jpg" alt="logo facebook">
+      <img class="logo-google" src="imagenes/logo-google.jpg" alt="logo Google">-->
+    </section>
+
 </footer>
